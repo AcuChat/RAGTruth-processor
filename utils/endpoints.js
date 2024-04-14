5f61b2742d059a8524dec5eccc66d193656f7704
Michael Wood <michaelwood33311@icloud.com>
2024-04-14 09:36:16 -0400

    added function to getData from table

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -7,3 +7,16 @@ exports.getTables = async (req, res) => {
 
     res.status(200).json(result);
 }
+
+exports.getData = async (req, res) => {
+    const { tableName } = req.body;
+
+    if (!tableName) return res.status(400).json({status: 'error', msg: "bad command"});
+
+    const q = `SELECT * from ${tableName}`;
+
+    const r = await mysql.query(q);
+    for (let i = 0; i < r.length; ++i) r[i].package = JSON.parse(r[i].package);
+
+    return res.status(200).json(r);
+}
