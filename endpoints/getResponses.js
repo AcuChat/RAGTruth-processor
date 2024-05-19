d2e6342642753c7cffd38aa732a96e282f58aa37
Michael Wood <michaelwood33311@icloud.com>
2024-05-19 09:26:18 -0400

    added getting responses by models, labels, and tasks

--- endpoints/getResponses.js
+++ endpoints/getResponses.js
@@ -1,14 +1,22 @@
 const data = require('../utils/data');
 
 exports.getResponses = async (req, res) => {
-    const { models, labels } = req.body;
+    const { models, labels, tasks } = req.body;
     if (!models) return res.status(400).json('bad request: missing models');
     if (!labels) return res.status(400).json('bad request: missing labels');
+    if (!tasks) return res.status(400).json('bad request: missing tasks');
     if (!models.length) return res.status(400).json('bad request: missing models');
     if (!labels.length) return res.status(400).json('bad request: missing labels');
+    if (!tasks.length) return res.status(400).json('bad request: missing tasks');
     
     const responseInfo = await data.getResponseInfo();
+    const sourceInfo = await data.getSourceInfo();
+
     const responses = [];
+
+    /**
+     * Filter by models, labels, and tasks
+     */
     responseInfo.forEach(ri => {
         let test = models.find(m => m === ri.model);
         if (!test) return;
@@ -20,6 +28,10 @@ exports.getResponses = async (req, res) => {
                 break;
             }
         }
+        if (!test) return;
+
+        const source = sourceInfo.find(si => si.source_id === ri.source_id);
+        test = tasks.find(task => task === source.task);
         if (!test) return;
         responses.push(ri);
     })
