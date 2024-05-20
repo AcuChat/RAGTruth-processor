c6ea8dd6f71eec9a5a74991529dff90b60f29909
Michael Wood <michaelwood33311@icloud.com>
2024-05-20 11:06:25 -0400

    added storing ragfix responses in db and serving from db

--- endpoints/getRagfixResponse.js
+++ endpoints/getRagfixResponse.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const sql = require('../utils/mysql');
 
 const backend = `https://api.ragfix.ai:5305`;
 
@@ -63,6 +64,12 @@ const queryUsingNounPhraseCollisionElimination = async (userPrompts, texts, mode
 exports.getRagfixResponse = async (req, res) => {
     const { responseId, passages, query, model } = req.body;
     if (!responseId) return res.status(400).json('bad command: missing responseId');
+
+    let q = `SELECT responses FROM responses WHERE response_id = ${sql.escape(responseId)}`;
+    let r = await sql.query(q);
+
+    if (r.length) return res.status(200).send(r[0].responses);
+
     if (!passages) return res.status(400).json('bad command: missing passages');
     if (!query) return res.status(400).json('bad command: missing query');
     if (!model) return res.status(400).json('bad command: missing model');
@@ -87,8 +94,10 @@ exports.getRagfixResponse = async (req, res) => {
     
     const responses = await queryUsingNounPhraseCollisionElimination(prompts, routes, model);
 
+    const ragfixResponses = {ragfixResponses: responses};
 
-    console.log('responses', responses)
+    q = `INSERT INTO responses (response_id, responses) VALUES (${sql.escape(responseId)}, ${sql.escape(JSON.stringify(ragfixResponses))})`;
+    r = await sql.query(q);
 
-    return res.status(200).json(responses);
+    return res.status(200).json(ragfixResponses);
 }
